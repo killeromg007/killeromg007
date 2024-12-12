2c7fbaff663d7d4f7c6af2e748f15bd1c1684d41
killeromg007 <154882148+killeromg007@users.noreply.github.com>
2024-12-12 18:21:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 
 // Smooth scrolling for navigation links
 window.onloadTurnstileCallback = function () {
-  turnstile.render("#example-container", {
-    sitekey: "<0x4AAAAAAA2B3HL2JkZAE-Hl>",
-    callback: function (token) {
-      console.log(`Challenge Success ${token}`);
-    },
-  });
-};
+    turnstile.render("#example-container", {
+      sitekey: "<0x4AAAAAAA2B3HL2JkZAE-Hl>",
+      callback: function (token) {
+        console.log(`Challenge Success ${token}`);
+        setTimeout(()  => {
+          document.getElementById("example-container").style.display = "none";
+        }, 2000);
+      },
+    });
+  };
 
 
 document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
